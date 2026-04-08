8e67ec4a84c0a06d63208dccb7172f5f3ebb16ce
Kenneth Yar <108104685+KennethJYS@users.noreply.github.com>
2026-04-07 20:03:30 -0500

    Zoowo cambio 3 (searchMovie)

--- providers/zoowomaniacos.js
+++ providers/zoowomaniacos.js
@@ -1,9 +1,24 @@
-var __create = Object.create;
 var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
 var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
@@ -16,14 +31,6 @@ var __copyProps = (to, from, except, desc) => {
   }
   return to;
 };
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-  // If the importer is in node compatibility mode or this is not an ESM
-  // file that has been converted to a CommonJS file using a Babel-
-  // compatible transform (i.e. "__esModule" has not been set), then set
-  // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-  mod
-));
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve2, reject) => {
@@ -50,7 +57,6 @@ __export(zoowomaniacos_exports, {
   getStreams: () => getStreams
 });
 module.exports = __toCommonJS(zoowomaniacos_exports);
-var import_axios = __toESM(require("axios"));
 var UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36";
 function resolve(embedUrl) {
   return __async(this, null, function* () {
@@ -135,20 +141,23 @@ function getTmdbData(tmdbId, mediaType) {
 function searchMovie(query) {
   return __async(this, null, function* () {
     try {
-      const { data } = yield import_axios.default.post(
-        `${BASE_URL}/alternativo3/server.php`,
-        new URLSearchParams({
-          "start": "0",
-          "length": "10",
-          "metodo": "ObtenerListaTotal",
-          "search[value]": query,
-          "searchPanes[a3][0]": "",
-          "searchPanes[a4][0]": "",
-          "searchPanes[a5][0]": "",
-          "searchPanes[a6][0]": ""
+      const formData = new URLSearchParams({
+        "start": "0",
+        "length": "10",
+        "metodo": "ObtenerListaTotal",
+        "search[value]": query,
+        "searchPanes[a3][0]": "",
+        "searchPanes[a4][0]": "",
+        "searchPanes[a5][0]": "",
+        "searchPanes[a6][0]": ""
+      });
+      const data = yield fetch(`${BASE_URL}/alternativo3/server.php`, {
+        method: "POST",
+        headers: __spreadProps(__spreadValues({}, HEADERS), {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
         }),
-        { timeout: 8e3, headers: HEADERS }
-      );
+        body: formData
+      }).then((r) => r.json());
       return (data == null ? void 0 : data.data) || [];
     } catch (e) {
       console.log(`[Zoowomaniacos] Error b\xFAsqueda: ${e.message}`);
@@ -267,3 +276,4 @@ function getStreams(tmdbId, mediaType) {
     }
   });
 }
+
